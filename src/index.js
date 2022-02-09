75b307080da3c774bf90ce7988c733187efa9771
Nilin <itsmenilin@gmail.com>
2022-02-09 11:43:13 +0530

    refetch added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider, } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right"/ >
     <App />
+    
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
