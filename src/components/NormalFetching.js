f03e8aaf406238891ac2a408006922cf6df7a5c2
Nilin <itsmenilin@gmail.com>
2022-02-09 09:21:42 +0530

    error handling added

--- src/components/NormalFetching.js
+++ src/components/NormalFetching.js
@@ -4,20 +4,36 @@ import React, { useEffect, useState } from 'react';
 function NormalFetching() {
   const [loading, setLoading] = useState(true);
   const [users, setUsers] = useState([]);
+  const [error,setError] = useState('');
 
   const fetchData = async () => {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-    const data = response.data;
-    setUsers(data);
-    setLoading(false);
+    
+    try{
+      const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+      const data = response.data;
+      setUsers(data);
+      setLoading(false)
+    }catch(err)
+    {  
+      setLoading(false)
+      setError(err.message)
+  
+    }
   }
+
+
   useEffect(() => {
     fetchData();
   }, [])
 
+
+
   return <div>
     {loading && <h1>Loading....</h1>}
 
+    {error && <h1>{error}</h1>}
+    
+    
     {
       users.map(user => (
         <div key={user.id}>
@@ -25,6 +41,11 @@ function NormalFetching() {
         </div>
       ))
     }
+      
+       
+     
+
+    
   </div>;
 }
 export default NormalFetching;
--- src/components/RQfetching.js
+++ src/components/RQfetching.js
@@ -5,13 +5,16 @@ import {useQuery} from 'react-query'
 function RQfetching() {
                   //queries have unique key in rq
                   //usequery accepts a fn that return a promise
-     const {data,isLoading}   =  useQuery("users",() => {
+     const {data,isLoading,isError,error}   =  useQuery("users",() => {
               return  axios.get("https://jsonplaceholder.typicode.com/users")
             })
+            console.log(error);
             
   return <div>
 
       {isLoading && <h1> Loading...</h1>}
+
+      {isError && <h1>{error.message}</h1>}
         
         {data?.data.map(user => (
             <div key={user.id}>
